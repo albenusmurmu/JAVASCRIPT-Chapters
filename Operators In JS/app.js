3b6d5ac1ce917abd54281fe107a4a02584b105bd
Albenus Pter Murmu <146658653+albenusmurmu@users.noreply.github.com>
2024-05-20 00:00:39 +0530

    JS-Chapter01

---
+++ Operators In JS/app.js
@@ -0,0 +1,77 @@
+// let tea=10;
+// let coffe=50;
+
+// *Template Litrals - to print a single line
+
+// let output=`The total price is: ${tea+coffe} Rupees.`;
+// console.log(output);
+
+// console.log(`The total price is: ${tea+coffe} Rupee.`);
+
+// *Operators in JS (Notes)
+//!Arithematics operators
+// let a=5;
+// let b=10;
+// console.log(a,b);
+// console.log(`${a}`);
+// console.log(`${b}`);
+// console.log(`${a+b}`);
+
+// console.log(a-b);
+
+// console.log(a*b);
+// console.log(a/b);
+
+// console.log(a%b);
+
+// console.log(a**b)
+//!Unary operators in JS(Notes)
+// console.log(a++)// a=5
+
+// console.log(++a)// a=7
+
+//! Assignment Operators (Notes)
+// a=10;
+// b=20;
+// a=b;
+
+// console.log("a=:",a);
+//! Comparison Operators 
+// let age = 18;
+// console.log(age >  18);//false
+// console.log(age < 18 );//false
+// console.log(age == 18 );//true
+// console.log(age >=  18);//true
+// console.log(age <= 18 );//true
+// console.log(age  != 2);//Not Equal to (true)
+// console.log(age == 18 );
+
+//* Conditions Statements
+
+// console.log("Before Using If statement");
+// let age= 10;
+// if(age>=18 ){
+//     console.log("You are eligible for vote");
+    // a=10;
+    // console.log(age > 18);
+    // console.log(`Enter Last amount is: ${10+a}`);
+
+// }
+// if (age<18){
+//     console.log("You are not eligible for vote");
+// }
+// console.log("After Using If statement");
+// !Practice Statements
+//Create a traffic light System theat shows what to do based on color.
+
+// let color = "yellow";
+// if (color=="red"){
+//     console.log("Stop! lighting is red");
+// }
+
+// if (color=="yellow"){
+//     console.log("Slow Down :lighting is yello");
+// }
+// if (color=="green"){
+//     console.log("Go :lighting is green");
+// }
