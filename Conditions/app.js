8d048109c38faa651812c6e36ce1b7f818dc3ee3
Albenus Pter Murmu <146658653+albenusmurmu@users.noreply.github.com>
2024-05-20 21:50:58 +0530

    Conditions-Statement

---
+++ Conditions/app.js
@@ -0,0 +1,142 @@
+//* Conditions Statements
+
+// console.log("Before Using If statement");
+// let age= 10;
+// if(age>=18 ){
+//     console.log("You are eligible for vote");
+    // a=10;
+    // console.log(age > 18);
+    // console.log(`Enter Last amount is: ${10+a}`);
+
+// }
+// if (age<18){
+//     console.log("You are not eligible for vote");
+// }
+// console.log("After Using If statement");
+// !Practice Statements
+//Create a traffic light System theat shows what to do based on color.
+
+// let color = "yellow";
+// if (color=="red"){
+//     console.log("Stop! lighting is red");
+// }
+
+// if (color=="yellow"){
+//     console.log("Slow Down :lighting is yello");
+// }
+// if (color=="green"){
+//     console.log("Go :lighting is green");
+// }
+
+// ! else if conditions
+// let a=29;
+// if(a>=21)
+// {
+//     console.log("you can merrage");
+
+// }
+// else if(a>=18)
+// {
+//     console.log("you can merrage and you can vote");
+// }
+// else if(a<=21){
+//     console.log("you can't merrage");
+// }
+// let marks = 90;
+// if(marks>=80)
+// {
+//     console.log("A+");
+// }
+// else if(marks>=70)
+// {
+//     console.log("A");
+// }
+// else if(marks>=33){
+//     console.log("b");
+// }
+// else if(marks<=33){
+//     console.log("fail");
+// }
+
+// *
+
+// let month="august";
+
+// if(month=="January"){
+//     console.log("It's winter Seasons");
+
+// }
+
+// else if(month=="march"){
+//     console.log("It's Summer Seasons");
+
+// }
+// else if(month=="august"){
+//     console.log("It's rainy Seasons")
+// }
+
+// let age = 33;
+// if(age>=20){
+//     console.log("You can Apply for Marriage certificate");
+
+// }
+// else if(age < 18){
+//     console.log("Yor are not eligible to apply a Marriage certificate");
+// }
+
+// else{
+//     console.log("You are teenager");
+// }
+
+// let color = "red";
+
+// if (color=="3"){
+//     console.log("Stop! lighting is red");
+// }
+
+//  else if (color=="yellow"){
+//     console.log("Slow Down :lighting is yello");
+// }
+// else if (color=="green"){
+//     console.log("Go :lighting is green");
+// }
+// else{
+//     console.log("Traffic light is broken");
+// }
+// // !PRACTICE Qs 
+// !-Create a system to calculate popcorn prices based on the size customer asked for:
+
+// let size = "S";
+// if(size==="XL"){
+//     console.log(`Rrice is:${250}`);
+
+// }
+// else if( size==="L"){
+//     console.log(`Rrice is:${200}`);
+// }
+
+// else if( size==="M"){
+//     console.log(`Price is:${100}`);
+// }
+
+// else if( size==="S"){
+//     console.log("price is:50");
+// }
+// else{
+//     console.log("Not avialable");
+// }
+// *
+// let price =100;
+
+// if(price<=110){
+//     console.log("best price range");
+//     if(price>=70){
+//         console.log("reasonable price range");
+//     }
+//     else{
+//         console.log("not affordable");
+//     }
+// }
+// else{
+//     console.log("worst price");
+// }
